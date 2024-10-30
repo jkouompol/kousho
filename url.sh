ede360490de93629637d11bee7ed1d989bb1fca4
newone211 <foujna66@proton.me>
2024-10-30 12:56:33 +0100

    Update url.sh

--- url.sh
+++ url.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
 
-# Generate SSH key (press Enter when prompted to accept defaults)
-ssh-keygen -t rsa -b 4096 -C "your_email@example.com" -f ~/.ssh/id_rsa -N ""
-
-# Start the SSH agent in the current shell session
 eval "$(ssh-agent -s)"
-
-# Add the SSH key to the agent
 ssh-add ~/.ssh/id_rsa
